12ecea70fb1aeff64d294d2572e59671445ed6a8
laura-jane <laura.kaipl@online.de>
2018-11-24 16:13:09 +0100

    config.interface.ts updated

--- src/config.interface.ts
+++ src/config.interface.ts
@@ -1,3 +1,9 @@
 export interface Config {
-
+    numberOfCats: number;
+    mixtureRatio: number;
+    constantNumber: number;
+    selfPositionConsidering: Boolean;
+    countsOfDimensionsToChange: number;
+    seekingRangeOfSelectedDimension: number;
+    seekingMemoryPool: number;
 }
