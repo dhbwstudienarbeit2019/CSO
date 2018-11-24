764ba8cc7575c5ad1b705ab5ec05be5886025f13
laura-jane <laura.kaipl@online.de>
2018-11-24 20:23:14 +0100

    function runCode() filled

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import {Config} from "./config.interface";
 import {Point, ResultMessage, StartMessage} from "./message.interface";
+import {Cat} from "./cat";
+import {Position} from "./Point";
+import {ClassicalSeekingMode} from "./ClassicalSeekingMode";
+import {ClassicalTracingMode} from "./ClassicalTracingMode";
+import * as MersenneTwister from 'mersenne-twister';
 
 addEventListener('message', (message: MessageEvent) => {
     try {
         let data = <StartMessage> message.data;
         config = data.config;
+        functionToOptimize = data.optimization;
+        searchDomain = data.searchDomain;
         if (data.action === 'abort') {
             isRunning = false;
         }
@@ -20,10 +27,63 @@ addEventListener('message', (message: MessageEvent) => {
 });
 
 let isRunning = false;
+let isFinished = false;
 
 let config: Config;
 
+let functionToOptimize: (point: Point) => number;
+
+let searchDomain: Position;
+
 function runCode(): Point[] {
-    // #TODO Your code should go here
-    return [];
+    let iterationCounter = 0;
+
+    let seekingMode = new ClassicalSeekingMode(config.seekingMemoryPool, config.seekingRangeOfSelectedDimension, config.countsOfDimensionsToChange, config.selfPositionConsidering);
+    let tracingMode = new ClassicalTracingMode(config.constantNumber, searchDomain);
+    let cats: Cat[];
+    let bestPosition: Position;
+    let results: Position[];
+    let fitnessValueBest = 1000;
+    let fitnessValueLeast = 0;
+    
+    for(let i = 0; i < config.numberOfCats; i++) {
+        this.cats[i] = new Cat(Position.doRandomPosition(), Position.doRandomPosition(), functionToOptimize);
+    }
+
+    while(iterationCounter < config.maximumNumberOfIterations) {
+
+        for(let i = 0; i < config.numberOfCats; i++) {
+            if(cats[i].calculateFitness() < fitnessValueBest) {
+                fitnessValueBest = cats[i].calculateFitness();
+                bestPosition = cats[i].Position;
+            }
+            if(cats[i].calculateFitness() > fitnessValueLeast) {
+                fitnessValueLeast = cats[i].calculateFitness();
+            }
+
+            const randomNumber = MersenneTwister.random();
+            if(randomNumber <= config.mixtureRatio) {
+                cats[i].Mode = false;
+            } else {
+                cats[i].Mode = true;
+            }
+        }
+        
+        results[iterationCounter] = bestPosition;
+
+        for(let i = 0; i < config.numberOfCats; i++) {
+            if(cats[i].Mode) {
+                seekingMode.seek(cats[i], fitnessValueBest, fitnessValueLeast);
+            } else {
+                tracingMode.trace(cats[i], bestPosition);
+            }
+        }
+
+        iterationCounter++;
+    }
+
+    isRunning = false;
+    isFinished = true;
+    
+    return results;
 }
