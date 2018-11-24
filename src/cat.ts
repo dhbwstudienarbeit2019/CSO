3119de42a2fdd2062cb35a0a099db0045c426959
laura-jane <laura.kaipl@online.de>
2018-11-24 20:25:50 +0100

    functionToOptimize added and calculateFitness() implemented

--- src/cat.ts
+++ src/cat.ts
@@ -1,16 +1,21 @@
 import {Position} from "./Point";
+import {Point} from "./message.interface";
 
 export class Cat {
     private position: Position;
-    private readonly fitnessValue: number;
     private velocity: Position;
     private mode: boolean;
     private selectionProperty: number;
+    private functionToOptimize: (point: Point) => number;
 
-    constructor(position: Position, velocity: Position) {
+    constructor(position: Position, velocity: Position, functionToOptimize: (point: Point) => number) {
         this.position = position;
-        this.fitnessValue = 10000;
         this.velocity = velocity;
+        this.functionToOptimize = functionToOptimize;
+    }
+
+    public get FunctionToOptimize(): ((point: Point) => number) {
+        return this.functionToOptimize;
     }
 
     public set Mode(mode: boolean) {
@@ -38,10 +43,7 @@ export class Cat {
     }
 
     calculateFitness(): number {
-
-        //TODO
-
-        return this.fitnessValue;
+        return this.functionToOptimize(this.position);
     }
 
     public set SelectionProb(selectionProb: number) {
