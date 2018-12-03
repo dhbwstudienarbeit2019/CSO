89a1f49de54774c2f43da76949ddca47b9fbd117
raspyweather <raspyweather@gmail.com>
2018-12-03 04:40:21 +0100

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const resolvedSchema = require('json-schema-loader');
+module.exports = {
+    mode: 'development',
+    entry: './src',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.json/,
+                exclude: /node_modules/,
+                loader: resolvedSchema
+            }
+
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 4201,
+        headers: { "Access-Control-Allow-Origin": "*" }
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: 'src/parameters.json', to: './parameters.json',
+            toType: 'file'
+        },
+            {
+                from: './README.md', to: './README.md',
+                toType: 'file'
+            }
+        ])]
+
+};
