70f24ba3a2cab3db0ace6a9444646d306ee0096f
raspyweather <raspyweather@gmail.com>
2018-12-03 04:41:05 +0100

    adjusted function call

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import {Config} from "./config.interface";
-import {Point, ResultMessage, StartMessage} from "./message.interface";
-import {Cat} from "./cat";
-import {Position} from "./Point";
-import {ClassicalSeekingMode} from "./ClassicalSeekingMode";
-import {ClassicalTracingMode} from "./ClassicalTracingMode";
+import { Config } from "./config.interface";
+import { Point, ResultMessage, StartMessage } from "./message.interface";
+import { Cat } from "./cat";
+import { Position } from "./Point";
+import { ClassicalSeekingMode } from "./ClassicalSeekingMode";
+import { ClassicalTracingMode } from "./ClassicalTracingMode";
 import * as MersenneTwister from 'mersenne-twister';
 
 addEventListener('message', (message: MessageEvent) => {
     try {
-        let data = <StartMessage> message.data;
+        let data = <StartMessage>message.data;
         config = data.config;
-        functionToOptimize = data.optimization;
+        functionToOptimize = new Function('return ' + data.optimization.toString())();
         searchDomain = data.searchDomain;
         if (data.action === 'abort') {
             isRunning = false;
@@ -22,7 +22,7 @@ addEventListener('message', (message: MessageEvent) => {
         }, undefined, undefined);
     }
     catch (e) {
-        postMessage(<ResultMessage>{info: e.toString(), result: [], status: "error"}, undefined, undefined);
+        postMessage(<ResultMessage>{ info: e.toString(), result: [], status: "error" }, undefined, undefined);
     }
 });
 
@@ -33,7 +33,7 @@ let config: Config;
 
 let functionToOptimize: (point: Point) => number;
 
-let searchDomain: {min: Point, max: Point};
+let searchDomain: { min: Point, max: Point };
 
 function runCode(): Point[] {
     let iterationCounter = 0;
@@ -46,34 +46,34 @@ function runCode(): Point[] {
     let results: Position[];
     let fitnessValueBest = 1000;
     let fitnessValueLeast = 0;
-    
-    for(let i = 0; i < config.numberOfCats; i++) {
+
+    for (let i = 0; i < config.numberOfCats; i++) {
         this.cats[i] = new Cat(Position.doRandomPosition(), Position.doRandomPosition(), functionToOptimize);
     }
 
-    while(!minimumReached && iterationCounter < config.maximumNumberOfIterations) {
+    while (!minimumReached && iterationCounter < config.maximumNumberOfIterations) {
 
-        for(let i = 0; i < config.numberOfCats; i++) {
-            if(cats[i].calculateFitness() < fitnessValueBest) {
+        for (let i = 0; i < config.numberOfCats; i++) {
+            if (cats[i].calculateFitness() < fitnessValueBest) {
                 fitnessValueBest = cats[i].calculateFitness();
                 bestPosition = cats[i].Position;
             }
-            if(cats[i].calculateFitness() > fitnessValueLeast) {
+            if (cats[i].calculateFitness() > fitnessValueLeast) {
                 fitnessValueLeast = cats[i].calculateFitness();
             }
 
             const randomNumber = MersenneTwister.random();
-            if(randomNumber <= config.mixtureRatio) {
+            if (randomNumber <= config.mixtureRatio) {
                 cats[i].Mode = false;
             } else {
                 cats[i].Mode = true;
             }
         }
-        
+
         results[iterationCounter] = bestPosition;
 
-        for(let i = 0; i < config.numberOfCats; i++) {
-            if(cats[i].Mode) {
+        for (let i = 0; i < config.numberOfCats; i++) {
+            if (cats[i].Mode) {
                 seekingMode.seek(cats[i], fitnessValueBest, fitnessValueLeast);
             } else {
                 tracingMode.trace(cats[i], bestPosition);
@@ -82,9 +82,9 @@ function runCode(): Point[] {
 
         iterationCounter++;
 
-        if(iterationCounter >= 40) {
-            for(let i = iterationCounter; i > (iterationCounter - 40);i--) {
-                if(results[i] != results[i-1]) {
+        if (iterationCounter >= 40) {
+            for (let i = iterationCounter; i > (iterationCounter - 40); i--) {
+                if (results[i] != results[i - 1]) {
                     minimumReached = false;
                     break;
                 } else {
@@ -93,9 +93,8 @@ function runCode(): Point[] {
             }
         }
     }
-
     isRunning = false;
     isFinished = true;
-    
+
     return results;
 }
