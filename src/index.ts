ee45c2b101f5847aaa141bcb1721e38d95d6760b
laura-jane <laura.kaipl@online.de>
2019-01-20 20:07:26 +0100

    logic for stopping if cats don't move no more implemented

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ function runCode(): Point[] {
     let cats: Cat[] = [];
     let lastResult: Position;
     let bestPosition: Position;
+    let lastTwentyCats: number[] = [];
     let results: Position[] = [];
     let fitnessValueBest = Number.POSITIVE_INFINITY;
     let fitnessValueLeast = Number.NEGATIVE_INFINITY;
@@ -78,6 +79,16 @@ function runCode(): Point[] {
                     console.log('no bestposition!');
                 }
                 tracingMode.trace(cats[i], bestPosition);
+            }
+        }
+        lastTwentyCats[iterationCounter % 20] = fitnessValueBest;
+        if (iterationCounter > 19) {
+            for (let i = 0; i < lastTwentyCats.length; i++) {
+                if (Math.abs(lastTwentyCats[i] - lastTwentyCats[i + 1]) > minimumEpsilon) {
+                    minimumReached = false;
+                    break;
+                }
+                minimumReached = true;
             }
         }
     }
